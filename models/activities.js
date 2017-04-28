968475d5b1cfc0c99fa7e62c1074ed52675f8f7c
j-hha <j-hha@bla.bla>
2017-04-28 18:04:10 -0400

    activitySchema added

---
+++ models/activities.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var activitySchema = new Schema({
+  creator: {type: Object, required: true},
+  title: {type: String, required: true},
+  description: {type: String, required: true},
+  typeOfExercise: String,
+  outdoor: {type: Boolean, required: true},
+  weather: {type: [ String ], required: true},
+  ageRange: {type: [ String ], required: true},
+  tags: [ String ]
+});
+
+var Activity = mongoose.model('Activity', activitySchema);
+
+module.exports = Activity;
