6bd1202336d099d8f6a4c5bbaf60af4663935d06
Geraldine Morales <geraldine.morales02@gmail.com>
2017-04-28 22:00:48 -0400

    Modified favorites array- is now an array of objects

--- models/users.js
+++ models/users.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
   username: {type: String, required: true, unique: true },
   password: {type: String, required: true},
-  favorites: [],
+  favorites: [Object],
   stats: [{
     child: String,
     points: Number,
