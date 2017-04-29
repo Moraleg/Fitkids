6d9d184bc8033e4408ebbebfb513bd98b0941fee
Geraldine Morales <geraldine.morales02@gmail.com>
2017-04-28 20:17:51 -0400

    Created User Model

---
+++ models/users.js
@@ -0,0 +1,17 @@
+//USER MODEL
+
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+  username: {type: String, required: true, unique: true },
+  password: {type: String, required: true},
+  favorites: [],
+  stats: [{
+    child: String,
+    points: Number,
+    completedActivities: [String]
+  }]
+});
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
