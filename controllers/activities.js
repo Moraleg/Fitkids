1f098f01aac0260cc24647aa406deb23277e09ae
j-hha <j-hha@bla.bla>
2017-04-28 21:58:25 -0400

    all routes written, but NOT tested yet

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,16 +1,145 @@
-// ------------------------ ROUTER SET UP ------------------------
+// ------------------------------- ROUTER SET UP -------------------------------
 
 var express = require('express'),
     router = express.Router(),
     Activity = require('../models/activities.js');
 
-// ------------------------ GET ROUTES ------------------------
+// -------------------------------- SEED ROUTE ---------------------------------
+// JUST FOR TESTING PURPOSES! NOTE: To make this work, I commented out the creator key in the activities model! Don't forget to revert!
 
-// ------------------------ POST ROUTE ------------------------
+var seedData = [
+  {
+    title: 'This is an exercise',
+    description: 'It is so much fun!',
+    typeOfExercise: 'aerobic',
+    outdoor: 'true',
+    weather: 'sunny',
+    ageRange: ['Toddler', 'Kindergardener'],
+    tags: ['fun', 'splash', 'splish']
+  },
+  {
+    title: 'This is another exercise',
+    description: 'It is so much more fun!',
+    typeOfExercise: 'anaerobic',
+    outdoor: 'false',
+    weather: 'rainy',
+    ageRange: ['Ten'],
+    tags: ['bla', 'splash', 'blo']
+  }
+];
 
-// ------------------------ PUT ROUTE ------------------------
+router.get('/seed', function (req, res) {
+  // add seed data to
+  Activity.insertMany(seedData, function(error, createdActivities) {
+    if (!error) {
+      // if no error occurs, redirect to activities routes
+      res.redirect('/activities')
+    } else {
+      // else send error
+      res.json(error);
+    }
+  });
+});
 
-// ------------------------ DELETE ROUTE ------------------------
+// ------------------------------- GET ROUTES ----------------------------------
+// tested
+router.get('/', function (req, res) {
+  // find all activities in the database
+  Activity.find({}, function (error, allActivities) {
+    if (!error) {
+      // if no error occurs, send array of all found database entries as json
+      res.json(allActivities);
+    } else {
+      // else send error
+      res.json(error);
+    }
+  });
+});
 
-// ------------------------ ROUTER EXPORT ------------------------
+// -------------------------------- POST ROUTE ---------------------------------
+// not tested
+router.post('/new', function (req, res)  {
+  // Create new database entry based on user input
+  Activity.create(req.body, function (error, createdActivity) {
+    if(!error) {
+      // if no error occurs, send json of createdActivity
+      res.json(createdActivity);
+    } else {
+      // else send error
+      res.json(error);
+    }
+  });
+});
+
+// --------------------------------- PUT ROUTE ---------------------------------
+// not tested
+
+// NOTE: Why not just findByIdAndUpdate: If we want to run authentication (--> is the user making the put request the creator of the database entry?), we'll need to compare the current user's id to the database entry's value for creator first. If they match, we will can allow an update, if not, we will want to send a 403. We cannot solely rely on the frontend for authentication, since you could also try to use curl to make a PUT request. This is why I opted for this update version. If you don't agree or know a better way to do it, let me know and I'll change it.
+
+router.put('/:id', function (req, res) {
+  // find activity to be updated
+  Activity.findById(req.params.id, function (error, foundActivity) {
+    // !! fill with correct code for authentication!!
+    // if logged-in user is author of activity, make update to database entry
+    // if (currentUserID === foundActivity.creator) {
+      Activity.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (error, updatedActivity) {
+        if(!error) {
+          // if no error occurs, send updated database entry as json
+          res.json(updatedActivity);
+        } else {
+          // else send error
+          res.json(error)
+        }
+      });
+      // if user is NOT the author of activity, send 403
+    //} else {
+      //res.status(403).send('Forbidden');
+    //}
+  });
+});
+
+// ------------------------------- DELETE ROUTE --------------------------------
+// not tested
+
+// NOTE: Why not just findByIdAndRemove: see put route
+
+router.delete('/:id', function (req, res) {
+  // find activity to be deleted
+  Activity.findById(req.params.id, function (error, foundActivity) {
+    // !! fill with correct code for authentication!!
+    // if logged-in user is the author of the activity, delete
+    // if (currentUserID === foundActivity.creator) {
+      Activity.findByIdAndRemove(req.params.id,
+        function (error, deletedActivity) {
+          // if no error occurs
+          if (!err) {
+            // //NOTE: the following will only work, if a user's favorites array holds the IDs of the liked activities! It will find
+            // // find all users
+            // User.find({}, function (error, allUsers) {
+            //   // loop through allUsers array
+            //   for (var i = 0; i < allUsers.length; i++) {
+            //     // loop through favorites array on User object
+            //     for (var j = 0; j < allUsers[i].favorites.length; j++) {
+            //       // if saved favorite is identical to id of deleted activity
+            //       if (allUsers[i].favorites[j] === deletedActivity._id) {
+            //         // remove this favorite from array
+            //         allUsers[i].favorites.splice(j, 1);
+            //       } // closes if
+            //     } // closes inner loop
+            //   } // closes outer loop
+            // }); // closes User.find() and callback
+            res.json(deletedActivity)
+          } else {
+            res.json(error);
+          }
+      });
+      // if user is NOT the author of activity, send 403
+    // } else {
+      //res.status(403).send('Forbidden');
+    // }
+  });
+});
+
+// ------------------------------- ROUTER EXPORT -------------------------------
+
 module.exports = router;
---
+++ server.js
@@ -0,0 +1,41 @@
+
+
+// DEPENDENCIES //
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/project_3';
+const db = mongoose.connection;
+const bodyParser = require('body-parser');
+// const session = require('express-session');
+const bcrypt = require('bcrypt');
+
+// CONTROLLERS //
+// const sessionsController = require('./controllers/sessions.js');
+// const usersController = require('./controllers/users.js');
+const activitiesController = require('./controllers/activities.js');
+
+
+// MIDDLEWARE //
+// app.use(express.static('public'));
+// app.use(bodyParser.json());
+// app.use(session({
+//   secret: "the-best-group-evva",
+//   resave: false,
+//   saveUninitialized: false
+// }));
+
+// CONTROLLER ROUTES //
+// app.use('/sessions/', sessionsController);
+// app.use('/users/', usersController);
+app.use('/activities/', activitiesController);
+
+// CONNECTION //
+mongoose.connect(mongoUri);
+db.once('open', function() {
+  console.log('project_3 connected to MongoDB!');
+  app.listen(port, function() {
+    console.log(`Server port ${port} listening...`);
+  });
+});
