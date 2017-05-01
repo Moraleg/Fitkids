1983b6a3b5244258e413779e64b024a0b00eb450
Geraldine Morales <geraldine.morales02@gmail.com>
2017-04-30 20:01:32 -0700

    updated authentication in user controller/ fixed syntax error for favorites array in user model

--- controllers/users.js
+++ controllers/users.js
@@ -8,10 +8,24 @@ var bcrypt = require('bcrypt');
 //================ROUTES=================
 
 //GET ROUTE
+//change for single user not every user
 //tested with curl and in the browser
 router.get('/', function(req, res){
   User.find({}, function(err, foundUsers){
     res.json(foundUsers);
+  });
+});
+//---------------------------------
+//get user by ID
+router.get('/:userId', function(req, res){
+  User.find({user: req.params.userId}, function(err, foundUsers){
+    // if (req.session.currentuser._id === foundUsers) {
+      if (!err) {
+        res.json(foundUsers);
+      } else {
+        res.json(err);
+      }
+    // }
   });
 });
 
@@ -25,19 +39,30 @@ router.post('/', function(req, res){
 });
 
 //PUT ROUTE
-//tested with curl
 router.put('/:id', function(req, res){
   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-  User.findByIdAndUpdate(req.params.id, req.body, {new:true}, function(err, updatedUsers){
-    res.json(updatedUsers);
+  User.findById(req.params.id, function(err, updatedUsers){
+    // if(req.sessions.currentuser._id === updatedUsers){
+      User.findByIdAndUpdate(req.params.id, req.body, {new:true}, function(err, updatedUsers){
+        if(!err) {
+          res.json(updatedUsers);
+        } else {
+          res.json(err);
+        }
+      });
+    // }
   });
 });
 
 //DELETE ROUTE
-//tested with curl
+//user delete route needs to loop through existing children and delete those who have this particular user's ObjectId as value listed under key 'parent'
 router.delete('/:id', function(req, res){
-  User.findByIdAndRemove(req.params.id, function(err, deletedUsers){
-    res.json(deletedUsers);
+  User.findById(req.params.id, function(err, deletedUsers){
+    if(req.session.currentuser._id === deletedUsers){
+      User.findByIdAndRemove(req.params.id, function(err, deletedUsers){
+        res.json(deletedUsers);
+      });
+    }
   });
 });
 
--- models/users.js
+++ models/users.js
@@ -1,16 +1,17 @@
 //USER MODEL
 
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-var userSchema = mongoose.Schema({
+var userSchema = new Schema({
   username: {type: String, required: true, unique: true },
   password: {type: String, required: true},
-  favorites: [ObjectId], 
-  stats: [{
-    child: String,
-    points: Number,
-    completedActivities: [String]
-  }]
+  favorites: [{type: Schema.Types.ObjectId}],
+  // stats: [{ //may not need this given children model
+  //   child: String,
+  //   points: Number,
+  //   completedActivities: [String]
+  // }]
 });
 
 var User = mongoose.model('User', userSchema);
