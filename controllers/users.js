ce5eb2cfafb92f8d79de77c3f46ecb455262f7f9
j-hha <j-hha@bla.bla>
2017-05-01 21:08:16 -0400

    added comments and cleaned up code

--- controllers/users.js
+++ controllers/users.js
@@ -17,11 +17,11 @@ router.get('/', function(req, res){
 });
 //---------------------------------
 //get user by ID
-router.get('/:userId', function(req, res){
-  User.find({user: req.params.userId}, function(err, foundUsers){
-    // if (req.session.currentuser._id === foundUsers) {
+router.get('/:id', function(req, res){
+  User.find({user: req.params.id}, function(err, foundUser){
+    // if (req.session.currentuser._id === foundUser._id) {
       if (!err) {
-        res.json(foundUsers);
+        res.json(foundUser);
       } else {
         res.json(err);
       }
@@ -33,8 +33,8 @@ router.get('/:userId', function(req, res){
 //tested with curl
 router.post('/', function(req, res){
   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-  User.create(req.body, function(err, createdUsers){
-    res.json(createdUsers);
+  User.create(req.body, function(err, createdUser){
+    res.json(createdUser);
   });
 });
 
@@ -46,40 +46,46 @@ router.patch('/:id', function(req, res) {
   User.findById(req.params.id, function(err, foundUser) {
     // check if session data identical to database entry --> authorization
     // if (req.sessions.currentuser._id === foundUser._id) {
-    // check if request body contains password information and if the password
-    // is at least 8 characters long ( --> also checked in front end, this is to
-    // cover curl requests)
-      if (req.body.password !== undefined && req.body.password.length >= 8) {
-        // encrypt password
-        req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-        // update user's password and respond with information as json
-        User.findByIdAndUpdate(req.params.id,
-          { $set: { password: req.body.password } }, {new:true},
-          function(err, updatedUser){
-            if(!err) {
-              res.json(updatedUser);
-            } else {
-              res.json(err);
-            }
-        });
-        // check if request body contains user name information and if username
-        // is not an empty string or only spaces ( --> also checked in front
-        // end, this is to cover curl requests)
-      } else if (req.body.username !== undefined &&
-        req.body.username.trim() !== '') {
-          // update user's username and respond with information as json
-          User.findByIdAndUpdate(req.params.id,
-          { $set: { username: req.body.username } }, {new:true},
-          function(err, updatedUser){
-            if(!err) {
-              res.json(updatedUser);
-            } else {
-              res.json(err);
-            }
-        });
+      if(!err) {
+        // check if request body contains password information and if the password
+        // is at least 8 characters long ( --> also checked in front end, this is to
+        // cover curl requests)
+          if (req.body.password !== undefined && req.body.password.length >= 8) {
+            // encrypt password
+            req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+            // update user's password and respond with information as json
+            User.findByIdAndUpdate(req.params.id,
+              { $set: { password: req.body.password } }, {new:true},
+              function(err, updatedUser){
+                if(!err) {
+                  res.json(updatedUser);
+                } else {
+                  // if error, send error
+                  res.json(err);
+                }
+            });
+            // check if request body contains user name information and if username
+            // is not an empty string or only spaces ( --> also checked in front
+            // end, this is to cover curl requests)
+          } else if (req.body.username !== undefined &&
+            req.body.username.trim() !== '') {
+              // update user's username and respond with information as json
+              User.findByIdAndUpdate(req.params.id,
+              { $set: { username: req.body.username } }, {new:true},
+              function(err, updatedUser){
+                if(!err) {
+                  res.json(updatedUser);
+                } else {
+                  // if error, send error
+                  res.json(err);
+                }
+            });
+          } else {
+            // do not allow any other patch requests to this route
+            res.status(403).send('Forbidden');
+          }
       } else {
-        // do not allow any other patch requests to this route
-        res.status(403).send('Forbidden');
+        res.json(err);
       }
     // } else {
     //   res.status(403).send('Forbidden');
@@ -90,15 +96,24 @@ router.patch('/:id', function(req, res) {
 //DELETE ROUTE
 //user delete route needs to loop through existing children and delete those who have this particular user's ObjectId as value listed under key 'parent'
 router.delete('/:id', function(req, res){
-  User.findById(req.params.id, function(err, deletedUsers){
-    if(req.session.currentuser._id === deletedUsers){
-      User.findByIdAndRemove(req.params.id, function(err, deletedUsers){
-        res.json(deletedUsers);
-      });
-    }
+  User.findById(req.params.id, function(err, deletedUser){
+    // if(req.session.currentuser._id === deletedUser._id){
+      if (!err) {
+        User.findByIdAndRemove(req.params.id, function(err, deletedUser){
+          if (!err) {
+            res.json(deletedUser);
+          } else {
+            res.json(err);
+          }
+        });
+      } else {
+        res.json(err);
+      }
+    // } else {
+    //   res.status(403).send('Forbidden');
+    // }
   });
 });
-
 
 
 module.exports = router;
