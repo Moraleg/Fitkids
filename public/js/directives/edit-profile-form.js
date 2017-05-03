5455394768b253bb0c5266144289c759cc6b6236
j-hha <j-hha@bla.bla>
2017-05-03 10:46:35 -0400

    clearing out before pulling from master to avoid merge conflicts

--- public/js/directives/edit-profile-form.js
+++
@@ -1,8 +0,0 @@
-angular.module('MyApp').directive('editProfileForm', function () {
-  return {
-    restrict: 'E',
-    templateUrl: '../../partials/edit-profile-form.html',
-    controller: 'editProfileFormController',
-    controllerAs: 'editProfileCtrl'
-  };
-});
