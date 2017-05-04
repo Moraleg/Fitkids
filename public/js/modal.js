a654f1d04bbc91f54f1861ef271516c457442d9b
Geraldine Morales <geraldine.morales02@gmail.com>
2017-05-03 23:23:31 -0700

    Added styling for activities and child progress

--- public/js/modal.js
+++ public/js/modal.js
@@ -2,15 +2,26 @@ console.log('modal.js is connected');
 $(function(){
   var $loginBtn = $('#open-login');
   var $signupBtn = $('#open-signup');
+  var $childBtn = $('#open-add-child');
+  console.log($childBtn);
+  var $editActivityBtn = $('#open-edit');
   var $activityBtn = $('#activity');
   console.log($activityBtn);
   var $login = $('#login-modal');
   var $signUp = $('#signup-modal');
+  var $child = $('#edit-user-modal')
   var $activity = $('#activity-modal');
   var $closeBtn= $('.close');
 
 
 //===============EVENT HANDLERS================/
+
+  //Event handlers to open and close create new activity
+  var editActivity = function(){
+    console.log('edit activity has been clicked');
+    // $activity.css('display', 'block');
+  };
+
   //Event handlers to open and close login
   var openLogin = function(){
     // console.log('Login has been clicked');
@@ -20,6 +31,8 @@ $(function(){
     // console.log('close has been clicked');
     $login.css('display', 'none');
     $signUp.css('display', 'none');
+    $activity.css('display', 'none');
+    $child.css('display', 'none');
   };
 
   //Event handlers to open and close sign up
@@ -44,8 +57,12 @@ $(function(){
   $signupBtn.on('click', openSignup);
   $closeBtn.on('click', closeModal);
 
-  //Event handlers for sign up
-  $activityBtn.on('click', openActivity);
-  $closeBtn.on('click', closeModal);
-
+  // //Event handlers for sign up
+  // $activityBtn.on('click', openActivity);
+  // $closeBtn.on('click', closeModal);
+  //
+  //
+  // //Event handlers for edit activity
+  // $editActivityBtn.on('click', editActivity);
+  // $closeBtn.on('click', closeModal);
 });
